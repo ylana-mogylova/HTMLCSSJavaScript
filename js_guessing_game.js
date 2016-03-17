ea4be5d2eb35981119166dffca96c99e0899c618
Uliana Mogylova <Uliana_Mogylova@epam.com>
2016-03-16 22:30:11 -0600

    final commit

--- js_guessing_game.js
+++ js_guessing_game.js
@@ -3,28 +3,29 @@ var guess_input_text;
 var finished = false;
 var guesses = 0;
 var index = 0;
-
-var colourArray = ['blue', 'yellow', 'white', 'brown', 'green', 'orange', 'purpole', 'black', 'grey', 'red', 'chocolate', 'pink', 'indigo', 'lime', 'magenta'];
+// colour array creation
+var colourArray = ['blue', 'yellow', 'white', 'brown', 'green', 'orange', 'purple', 'black', 'grey', 'red', 'chocolate', 'pink', 'indigo', 'lime', 'magenta'];
 
 function do_game() {
-
-    var random_number = Math.random() * 15;
+    // generate a random number from 0 to 14.999999999
+    var random_number = Math.random() * colourArray.length;
+    // round a generated number and add 1 to have range from 0 to colourArray length
     var random_number_integer = Math.floor(random_number)+1;
+    // store targer colour
     target = colourArray[random_number_integer];
-    colourArray.sort();
-    console.log(target);
-    guess_input_text = prompt("I am thinking of one of these colours: "+
-                          show_colour(0)+"\n\n"+
-                          "What is the number?");
 
-    /*while (!finished) {
+    // sort array in the alphabetical order
+    colourArray.sort();
+    //console.log(target);
+
+
+    while (!finished) {
         guess_input_text = prompt("I am thinking of one of these colours: "+
                               show_colour(0)+"\n\n"+
-                              "What is the number?");
-        //guess_input = parseInt(guess_input_text);
+                              "What color am I thinking of?");
         guesses += 1;
         finished = check_guess();
-    }*/
+    }
 
 }
 
@@ -35,4 +36,65 @@ function show_colour(index){
       } else return colourArray[index] + show_colour(index+1)
     } else
       return "";
+}
+
+function check_guess(){
+  /*  if (colourArray.indexOf(guess_input_text)==-1) {
+        alert("Sorry, I do not recognize your colour \n \n"+
+              "Please try again.");
+        return false;
+    }
+
+    if (guess_input_text.localeCompare(target) == -1) {
+        alert("Sorry, your guess is not correct. \n\n" +
+              "Hint: your colour is alphabetically lower than mine! \n \n"+
+              "Try again.");
+        return false;
+    }
+
+    if (guess_input_text.localeCompare(target) == 1) {
+      alert("Sorry, your guess is not correct. \n\n" +
+            "Hint: your colour is alphabetically higher than mine! \n \n"+
+            "Try again.");
+        return false;
+    }*/
+    if(target == null || guess_input_text == null){
+      return true;
+    }
+    var height = target.localeCompare(guess_input_text);
+    switch(height){
+      case -1:
+        alert("Sorry, your guess is not correct. \n\n" +
+              "Hint: your colour is alphabetically lower than mine! \n \n"+
+              "Try again.");
+        return false;
+        break;
+      case 0:
+        document.body.style.backgroundColor = target;
+        alert("Congratulations! You have guessed the colour!\n\n"+ "The colour was " + target +
+              ".\n\n It took you " + guesses +
+              " guesses to get the colour!");
+        return true;
+        break;
+      case 1:
+        if (colourArray.indexOf(guess_input_text)==-1) {
+              alert("Sorry, I do not recognize your colour \n \n"+
+                    "Please try again.");
+              return false;
+        }else{
+          alert("Sorry, your guess is not correct. \n\n" +
+              "Hint: your colour is alphabetically higher than mine! \n \n"+
+              "Try again.");
+          return false;
+        }
+        break;
+      default:
+        alert("Sorry, your guess is not correct. \n\n" +
+              "Hint: your colour is alphabetically lower than mine! \n \n"+
+              "Try again.");
+        return false;
+        break;
+    }
+
+
 }
